1631f60a8181331c792df85e82b5b95110cf9751
Alex Vervloet <alexvervloet@Alexs-MacBook-Pro.local>
2018-05-30 06:26:23 +0800

    Alpha: Set up basics of front end logic and components

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -3,12 +3,16 @@ const {
   GraphQLObjectType,
   GraphQLID
 } = graphql;
+const UserType = require('./user_type')
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    none: {
-      type: GraphQLID
+    user: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        return req.user;
+      }
     }
   }
 });
